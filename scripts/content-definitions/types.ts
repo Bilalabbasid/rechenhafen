c6fcbbdcbc784c9b6a0c33fe31db13b9a6781b19
Bilal Abbasi <abbasi.bilal2000@gmail.com>
2026-09-23 09:22:09 +0500

    Clean repository: remove temporary development and test scripts

--- scripts/content-definitions/types.ts
+++
@@ -1,5 +0,0 @@
-export interface CalcContent {
-  intro: string;
-  details: string;
-  faqs: Array<{ question: string; answer: string }>;
-}
--- scripts/find-broken-related.ts
+++
@@ -1,9 +0,0 @@
-import { ALL_CALCULATORS, getCalculatorBySlug } from '../src/data/calculators';
-
-for (const c of ALL_CALCULATORS) {
-  for (const rel of c.relatedSlugs) {
-    if (!getCalculatorBySlug(rel)) {
-      console.log(`BROKEN in '${c.slug}': '${rel}'`);
-    }
-  }
-}
